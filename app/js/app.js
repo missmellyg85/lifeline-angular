ddb52e3511099e458a258b8d68d88e3ffc2d3f04
missmellyg85 <missywilliams85@gmail.com>
2014-02-10 22:29:59 -0600

    Put some pages together. Still missing a lot of content

--- app/js/app.js
+++ app/js/app.js
@@ -21,10 +21,15 @@ config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/client-contact', {templateUrl: 'partials/client/client-contact.html', controller: 'Client'});
 
   $routeProvider.when('/supporter-index', {templateUrl: 'partials/supporter/supporter-index.html', controller: 'Supporter'});
-  $routeProvider.when('/supporter-community', {templateUrl: 'partials/supporter/supporter-community.html', controller: 'Supporter'});
+  $routeProvider.when('/supporter-get-involved', {templateUrl: 'partials/supporter/supporter-get-involved.html', controller: 'Supporter'});
   $routeProvider.when('/supporter-team', {templateUrl: 'partials/supporter/supporter-team.html', controller: 'Supporter'});
   $routeProvider.when('/supporter-services/', {templateUrl: 'partials/supporter/supporter-services.html', controller: 'Supporter'});
   $routeProvider.when('/supporter-services/:service', {templateUrl: 'partials/supporter/supporter-services.html', controller: 'Supporter'});
+  
+  $routeProvider.when('/supporter-learn-more', {templateUrl: 'partials/supporter/supporter-learn-more.html', controller: 'Supporter'});
+  $routeProvider.when('/supporter-donate', {templateUrl: 'partials/supporter/supporter-donate.html', controller: 'Supporter'});
+  $routeProvider.when('/supporter-contact', {templateUrl: 'partials/supporter/supporter-contact.html', controller: 'Supporter'});
+  $routeProvider.when('/supporter-stories', {templateUrl: 'partials/supporter/supporter-stories.html', controller: 'Supporter'});
   
   $routeProvider.otherwise({redirectTo: '/client-index'});
 }]);
