71b3ef9805b186b59c393b0e5bd40e1f876d7671
missmellyg85 <missywilliams85@gmail.com>
2014-02-28 14:17:26 -0600

    Reorg some pages. Add you Add youtube video. Remove some menu items that didnt seem to be organized well.

--- app/js/app.js
+++ app/js/app.js
@@ -26,10 +26,13 @@ config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/supporter-services/', {templateUrl: 'partials/supporter/supporter-services.html', controller: 'Supporter'});
   $routeProvider.when('/supporter-services/:service', {templateUrl: 'partials/supporter/supporter-services.html', controller: 'Supporter'});
   
-  $routeProvider.when('/supporter-learn-more', {templateUrl: 'partials/supporter/supporter-learn-more.html', controller: 'Supporter'});
+  $routeProvider.when('/supporter-about', {templateUrl: 'partials/supporter/supporter-about.html', controller: 'Supporter'});
   $routeProvider.when('/supporter-donate', {templateUrl: 'partials/supporter/supporter-donate.html', controller: 'Supporter'});
   $routeProvider.when('/supporter-contact', {templateUrl: 'partials/supporter/supporter-contact.html', controller: 'Supporter'});
   $routeProvider.when('/supporter-stories', {templateUrl: 'partials/supporter/supporter-stories.html', controller: 'Supporter'});
+  $routeProvider.when('/supporter-videos', {templateUrl: 'partials/supporter/supporter-videos.html', controller: 'Supporter'});
+  $routeProvider.when('/supporter-events', {templateUrl: 'partials/supporter/supporter-events.html', controller: 'Supporter'});
+  $routeProvider.when('/supporter-new-projects', {templateUrl: 'partials/supporter/supporter-new-projects.html', controller: 'Supporter'});
   
   $routeProvider.otherwise({redirectTo: '/client-index'});
 }]);
