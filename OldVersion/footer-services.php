5c3156b5d1cf4a63818607541d52409f59e43d7d
Melissa Williams <missywilliams@Melissas-MacBook-Pro.local>
2015-10-12 19:57:57 -0500

    I guess I deleted a lot of stuff

--- OldVersion/footer-services.php
+++
@@ -1,8 +0,0 @@
-	<hr/>
-			<div class="row" id="footer">
-			<div class="span12">Copyright &#169; 2013 - The content of this website is copyright of Lifeline Pregnancy Help Clinic and may not be copied without permission.
-			</div>
-		</div>
-	</div>
-</body>
-</html>
--- OldVersion/services/peer.php
+++
@@ -1,8 +0,0 @@
-<div class="row">
-	<div class="span12">
-		<a href="peer"></a>
-		<img src="img/services/peer_counseling.jpg" style="padding: 0 10px 0 10px;float:left;" />
-		<h3>Peer Counseling</h3>
-		<p>With everything you have to think about, you may feel like no one understands your situation right now. Lifeline is here to listen without judgment. You can meet one-on-one with a peer counselor who will help you process information, your feelings, and the choices ahead. The decision of how to handle your pregnancy is up to you; Lifeline wants for you to be able to make that decision confidently, and with our understanding and support.</p>
-	</div>
-</div>
